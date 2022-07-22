76e266164a1dce5fa53d628c2cc93e45e39c6150
FirozTheProgrammer <firozhasan1542@gmail.com>
2022-07-22 13:56:34 +0600

    JSON

---
+++ README.md
@@ -0,0 +1,2 @@
+# JSON-DEMO
+# JSON-DEMO
---
+++ app.js
@@ -0,0 +1,19 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => displayUser(json));
+
+function displayUser(users) {
+  const userNames = users.map((userr) => userr.username);
+  console.log(userNames);
+
+  var ul = document.querySelector(".user_container");
+
+  for (let i = 0; i < userNames.length; i++) {
+    const name = userNames[i];
+    const li = document.createElement("li");
+    li.innerHTML = name;
+    ul.appendChild(li);
+  }
+}
+
+document.querySelector(".demo").innerHTML = new Date();
